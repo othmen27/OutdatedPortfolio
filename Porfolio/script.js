ca231397f066db0bba956c98ac02b5ed69a3df3f
othmen27 <39593870+othmen27@users.noreply.github.com>
2025-03-06 01:48:32 +0100

    Delete Porfolio directory

--- Porfolio/script.js
+++
@@ -1,7 +0,0 @@
-document.querySelectorAll('nav a').forEach(anchor => {
-    anchor.addEventListener('click', function(event) {
-        event.preventDefault();
-        const targetSection = document.querySelector(this.getAttribute('href'));
-        targetSection.scrollIntoView({ behavior: 'smooth' });
-    });
-});
